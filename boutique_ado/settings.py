59b5fe35d69b674dd79dabedf347f7ba4c911694
Alan Bushell <alanbushell91@gmail.com>
2022-11-23 05:57:30 +0000

    remove secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fjduc@5e@vl(p#*fn2w#nx5sirvkm1(9b-1%ija@z4v8w^2826'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
